0accf5655e2c55f815a219cb2db9cf4499dc4655
Novandra Wichda Farun <vandrasurabaya@gmail.com>
2025-11-07 22:33:15 +0700

    feat: enhance multer storage configuration to create destination folder if it doesn't exist

--- server/config/uploud.js
+++ server/config/uploud.js
@@ -1,11 +1,20 @@
 import multer from "multer";
 import path from "path";
+import fs from "fs";
 
 //* Konfigurasi penyimpanan untuk Multer */
+const publicImagesPath = path.join(process.cwd(), "public", "images");
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     //! Menentukan folder tujuan penyimpanan file
-    cb(null, "public/images/");
+    try {
+      // create the folder if it doesn't exist (no-op if it does)
+      fs.mkdirSync(publicImagesPath, { recursive: true });
+      cb(null, publicImagesPath);
+    } catch (err) {
+      cb(err);
+    }
   },
   filename: function (req, file, cb) {
     //! Membuat nama file yang unik untuk menghindari konflik
