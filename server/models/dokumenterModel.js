2fddd586a9592276c57a63081d2ccc3edcf5e393
Novandra Wichda Farun <vandrasurabaya@gmail.com>
2025-11-08 14:49:50 +0700

    fix: restore default value for date field in dokumenter model

--- server/models/dokumenterModel.js
+++ server/models/dokumenterModel.js
@@ -5,7 +5,7 @@ const dokumenterSchema = new mongoose.Schema({
   title: { type: String, required: true },
   imageUrl: { type: String, required: true },
   description: { type: String, required: true },
-  date: { type: Date },
+  date: { type: Date, default: Date.now },
 });
 
 const dokumenterModel =
