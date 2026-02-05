f89739311fe1f2b9eb3950a68ab9ed4bfd63b310
Novandra Wichda Farun <vandrasurabaya@gmail.com>
2026-02-05 13:24:34 +0700

    role back

--- server/server.js
+++ server/server.js
@@ -31,20 +31,7 @@ const allowedOrigins = [
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does not allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-    credentials: true,
-  }),
-);
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 //* API Endpoints
